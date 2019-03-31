462a9e6e8478fad7a47528d63fdc5c4602fb59c8
Edward Parker <edward@albany.me.uk>
2019-03-31 11:40:49 +0100

    After db

--- morningprayers_project/morningprayers_project/settings.py
+++ morningprayers_project/morningprayers_project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todo',
+   # 'todo',
+    'todo.apps.TodoConfig',
 ]
 
 MIDDLEWARE = [
@@ -104,10 +105,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-uk'
 
-TIME_ZONE = 'UTC'
-
+# TIME_ZONE = 'UTC'
+TIME_ZONE = 'GB'
 USE_I18N = True
 
 USE_L10N = True
